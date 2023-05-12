3f798f8ed7c615a506e306795ddb37d3e5a7fc0f
Building Surveys Dev <buildingsurveysdevgitdevgit@buildingsurveys.net>
2023-05-12 10:13:33 -0400

    feat: request the LS database

--- DictionaryLook.php
+++ DictionaryLook.php
@@ -22,9 +22,11 @@ class DictionaryLook extends PluginBase {
 
     public function init()
     {
+        $this->subscribe('beforeSurveySettings');
         $this->subscribe('beforeSurveyPage');
         $this->subscribe('beforeCloseHtml');
         $this->subscribe('newDirectRequest');
+        $this->subscribe('newSurveySettings');
     }
 
     public function beforeSurveyPage()
@@ -62,6 +64,85 @@ class DictionaryLook extends PluginBase {
         $client->registerScriptFile($this->getPluginFileUrl('js/language.js'));
         $client->registerScriptFile($this->getPluginFileUrl('js/dictLookup.js'));
         $client->registerCssFile($this->getPluginFileUrl('css/dictLookup.css'));
+    }
+
+    /**
+     * Survey Settings
+     */
+    public function beforeSurveySettings()
+    {
+        $event = $this->event;
+        
+        $newSettings = array(
+            'dictionarySurvey' => array(
+                'type'=>'select',
+                'options' => $this->beforeSurveySettings_SurveySelectList(),
+                'htmlOptions'=>array(
+                    'empty'=>$this->t("None"),
+                ),
+                'label' => $this->t('Dictionary Survey:'),
+                'current' => $this->beforeSurveySettings_Current('dictionarySurvey'),
+                'help' => 'Source survey where terms and definitions can be found',
+            ),
+            'termQuestionCode' => array(
+                'type' => 'string',
+                'label' => $this->t('Term question:'),
+                'current' => $this->beforeSurveySettings_Current('termQuestionCode'),
+                'help' => 'Question code of the question holding the terms',
+            ),
+            'definitionQuestionCode' => array(
+                'type' => 'string',
+                'label' => $this->t('Definition question:'),
+                'current' => $this->beforeSurveySettings_Current('definitionQuestionCode'),
+                'help' => 'Question code of the question holding the definitions',
+            ),            
+        );
+        // echo $this->beforeSurveySettings_Current('dictionarySurvey');
+        // Set all settings
+        $event->set("surveysettings.{$this->id}", array(
+            'name' => get_class($this),
+            'settings' => $newSettings,
+        ));
+    }
+
+    public function newSurveySettings()
+    {
+        $event = $this->event;
+
+        foreach ($event->get('settings') as $name => $value)
+        {
+            $this->set($name, $value, 'Survey', $event->get('survey'));
+        }
+    }
+
+    protected function beforeSurveySettings_SurveySelectList($curSid = NULL)
+    {
+        if (empty($curSid)) $curSid = $this->event->get('survey');
+
+        // Get the survey list according to user permissions
+        $oSurvey = new \Survey;
+        $oSurvey->permission(\Yii::app()->user->getId());
+        $aoSurveys = $oSurvey->with(
+            array(
+                'languagesettings' => array('condition'=>'surveyls_language=language'),
+                'owner'
+            )
+        )->findAll("sid <> :sid",array(":sid"=>$curSid));
+
+        // Compose list
+        $aSurveys = [];
+        foreach($aoSurveys as $S)
+        {
+            $aSurveys[ $S->sid ]= "[". $S->sid . "] " . $S->LocalizedTitle;
+        }
+
+        return $aSurveys;
+    }
+
+    public function beforeSurveySettings_Current($setting)
+    {
+        $event = $this->event;
+        return $this->get($setting, 'Survey', $event->get('survey'), $this->get('setting'));
     }
 
     public function beforeCloseHtml()
@@ -124,6 +205,10 @@ class DictionaryLook extends PluginBase {
 
     public function lookup()
     {
+        if (empty($_GET['l'])) die('No language defined');
+        $termList = $this->getTerms($_GET['surveyId']);
+        return $termList;
+
         $lang = $_GET['l'];
         $defs = include 'php/dictionary-'.$lang.'.php';
         $defs = array_change_key_case($defs, CASE_LOWER);
@@ -134,6 +219,11 @@ class DictionaryLook extends PluginBase {
 
     public function searchWord()
     {
+        $term = $_GET['t'];
+        $lang = $_GET['l'];
+        $definitionTerm = $this->getDefnitions($_GET['surveyId'], $term);
+        return $definitionTerm;
+
         $defs = include 'php/dictionary-'.$lang.'.php';
 
         $defs = array_change_key_case($defs, CASE_LOWER);
@@ -177,4 +267,75 @@ class DictionaryLook extends PluginBase {
         return $this->getPluginDir() . '/' . $relativePath;
     }
 
+    public function getTerms($surveyId)
+    {
+        $termResponse = [];
+        $dictionarySurveyId = $this->get('dictionarySurvey', 'Survey', $surveyId);
+        // $questionId = $this->get('termQuestionCode', 'Survey', $surveyId);        
+        $response = \SurveyDynamic::model($dictionarySurveyId)->findAll();
+        $sourceQuestion = Question::model()->findByAttributes(array('sid' => $dictionarySurveyId));
+        $questionColumnCode = $this->getSGQ($sourceQuestion);
+
+        foreach($response as $value){
+            array_push($termResponse, $value[$questionColumnCode]);
+        }
+
+        return CJSON::encode($termResponse);
+    }
+
+    public function getDefnitions($surveyId, $term)
+    {
+        $dictionarySurveyId = $this->get('dictionarySurvey', 'Survey', $surveyId);
+        $sourceQuestion = Question::model()->findByAttributes(
+            array(
+                'sid' => $dictionarySurveyId,
+            )
+        );
+        $sourceTerm = Question::model()->findByAttributes(
+            array(
+                'sid' => $dictionarySurveyId,
+                'title' => 'def',
+            )
+        );
+        $questionColumnCode = $this->getSGQ($sourceQuestion);
+        $termColumnCode = $this->getSGQ($sourceTerm);
+
+        $definitionRaw = SurveyDynamic::model($dictionarySurveyId)->findByAttributes(
+            array(
+                $questionColumnCode => $term,
+            )
+        );
+
+        return $definitionRaw[$termColumnCode];
+    }
+
+    // Get a SGQ from a Question
+    public static function getSGQ($Q)
+    {
+        if (!empty($Q->parent_qid))
+            $sgq = $Q->sid.'X'.$Q->gid.'X'.$Q->parent_qid."".$Q->title;
+        else
+            $sgq = $Q->sid.'X'.$Q->gid.'X'.$Q->qid;
+        return $sgq;
+    }
+
+    protected function t($string)
+    {
+        $messageSource = get_class($this).'Lang';
+        return \Yii::t('',$string,array(),$messageSource);
+    }
+    
+    /**
+     * Loads tranlsations on own namespace
+     */
+    public function loadTranslations($basePath = NULL)
+    {
+        if (empty($basePath)) $basePath = __DIR__
+                                            . DIRECTORY_SEPARATOR .'..'
+                                            . DIRECTORY_SEPARATOR . '..'
+                                            . DIRECTORY_SEPARATOR . get_class($this) . '/locale';
+        // var_dump($basePath); die();
+
+        // messageSource for this plugin:
+    }
 }
