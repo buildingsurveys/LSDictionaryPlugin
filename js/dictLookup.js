f78ed5a980433612b24d9376052298db1a142b87
Building Surveys Dev <buildingsurveysdevgitdevgit@buildingsurveys.net>
2023-05-16 09:03:59 -0400

    fix format

--- js/dictLookup.js
+++ js/dictLookup.js
@@ -1,60 +1,57 @@
-//COMO HACER UNA CACHE
-
-/* CERAR OBJETO CON CONST */
 $(document).ready(function (){
     console.log('dictLook');
-        const popoverTemplate = `
-            <div class="popover" role="tooltip">
-                <div class="arrow"></div>
-                <h3 class="popover-title popover-header"></h3>
-                <div class="popover-content"></div>
-            </div>
-        `;
-        $(document).on('click', function (evt) {
-            const selectedElement = $(evt.target);
-            if ($('.popover').has(selectedElement).length === 0) {
-                $('.popover').popover('hide');
-            }
+    const popoverTemplate = `
+        <div class="popover" role="tooltip">
+            <div class="arrow"></div>
+            <h3 class="popover-title popover-header"></h3>
+            <div class="popover-content"></div>
+        </div>
+    `;
+    $(document).on('click', function (evt) {
+        const selectedElement = $(evt.target);
+        if ($('.popover').has(selectedElement).length === 0) {
+            $('.popover').popover('hide');
+        }
+
+        if(selectedElement.hasClass('info-icon')){
+            const dictLookupElement = selectedElement;
+            const parentElement = selectedElement.parent();
+
+            /**
+            * Init
+            */
+            let term = replaceNbsps(parentElement.text());
+
+            /**
+             * Get Term from Data?
+             */
+            let dataTerm = parentElement.attr('data-term');
+            if (typeof dataTerm != 'undefined') term = dataTerm;
+
+            // Compose Title
+            let title = term.substring(0, 3) != 'REF' ? term : 'Reference #' +  term.substring(3);
+            let lang = GetCurrPageLang();
+            console.log("Setting up popover for " + term, selectedElement);
+            $.ajax({
+                url: _dictUrl + '&t=' + term + '&l=' + lang
+            })
+            .then(function(content) {
+                // Set the tooltip content upon successful retrieval
+    //AQUI SE ASIGNA Cnombre_const ()
+                const popoverWithTerm = dictLookupElement.popover(defaultPopoverOptions(
+                    {
+                        title,
+                        html: true,
+                        placement: 'auto',
+                        content,
+                        template: popoverTemplate,
+                    })
+                ).popover('show');
     
-            if(selectedElement.hasClass('info-icon')){
-                const dictLookupElement = selectedElement;
-                const parentElement = selectedElement.parent();
-    
-                /**
-                * Init
-                */
-                let term = replaceNbsps(parentElement.text());
-    
-                /**
-                 * Get Term from Data?
-                 */
-                let dataTerm = parentElement.attr('data-term');
-                if (typeof dataTerm != 'undefined') term = dataTerm;
-    
-                // Compose Title
-                let title = term.substring(0, 3) != 'REF' ? term : 'Reference #' +  term.substring(3);
-                let lang = GetCurrPageLang();
-                console.log("Setting up popover for " + term, selectedElement);
-                $.ajax({
-                    url: _dictUrl + '&t=' + term + '&l=' + lang
-                })
-                .then(function(content) {
-                    // Set the tooltip content upon successful retrieval
-        //AQUI SE ASIGNA Cnombre_const ()
-                    const popoverWithTerm = dictLookupElement.popover(defaultPopoverOptions(
-                        {
-                            title,
-                            html: true,
-                            placement: 'auto',
-                            content,
-                            template: popoverTemplate,
-                        })
-                    ).popover('show');
-        
-                });
-            }
-        });
-    
+            });
+        }
+    });
+
     function defaultPopoverOptions({content, title, placement = 'top', trigger = 'manual', ...rest}) {
         return {
             content,
