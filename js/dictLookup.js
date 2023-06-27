c5e05be75c0e880227bf0059987dcc67479b0025
Building Surveys Dev <buildingsurveysdevgitdevgit@buildingsurveys.net>
2023-06-27 12:48:29 -0400

    add event for each element

--- js/dictLookup.js
+++ js/dictLookup.js
@@ -7,52 +7,55 @@ $(document).ready(function (){
             <div class="popover-content popover-body"></div>
         </div>
     `;
+
     $(document).on('click', function (evt) {
         const selectedElement = $(evt.target);
         if ($('.popover').has(selectedElement).length === 0) {
             $('.popover').popover('hide');
         }
-
-        if(selectedElement.hasClass('info-icon') || selectedElement.hasClass('dictLookup')){
-            evt.stopPropagation();
-            evt.preventDefault();
-            const dictLookupElement = selectedElement;
-            const elementWithTerm = selectedElement.hasClass('dictLookup') ?
-                selectedElement :
-                selectedElement.parent();
-
-            /**
-            * Init
-            */
-            let term = replaceNbsps(elementWithTerm.text());
-
-            /**
-             * Get Term from Data?
-             */
-            let dataTerm = elementWithTerm.attr('data-term');
-            if (typeof dataTerm != 'undefined') term = dataTerm;
-
-            // Compose Title
-            let title = term.substring(0, 3) != 'REF' ? term : 'Reference #' +  term.substring(3);
-            let lang = GetCurrPageLang();
-            console.log("Setting up popover for " + term, selectedElement);
-            $.ajax({
-                url: _dictUrl + '&t=' + title + '&l=' + lang
-            })
-            .then(function(content) {
-                // Set the tooltip content upon successful retrieval
-                dictLookupElement.popover(defaultPopoverOptions(
-                    {
-                        title,
-                        html: true,
-                        placement: 'auto',
-                        content,
-                        template: popoverTemplate,
-                    })
-                ).popover('show');
+    });
     
-            });
-        }
+    $('.info-icon, .dictLookup').on('click', function (evt) {
+        evt.stopPropagation();
+        evt.preventDefault();
+
+        const selectedElement = $(evt.target);
+        const dictLookupElement = selectedElement;
+        const elementWithTerm = selectedElement.hasClass('dictLookup') ?
+            selectedElement :
+            selectedElement.parent();
+
+        /**
+        * Init
+        */
+        let term = replaceNbsps(elementWithTerm.text());
+
+        /**
+         * Get Term from Data?
+         */
+        let dataTerm = elementWithTerm.attr('data-term');
+        if (typeof dataTerm != 'undefined') term = dataTerm;
+
+        // Compose Title
+        let title = term.substring(0, 3) != 'REF' ? term : 'Reference #' +  term.substring(3);
+        let lang = GetCurrPageLang();
+        console.log("Setting up popover for " + term, selectedElement);
+        $.ajax({
+            url: _dictUrl + '&t=' + title + '&l=' + lang
+        })
+        .then(function(content) {
+            // Set the tooltip content upon successful retrieval
+            dictLookupElement.popover(defaultPopoverOptions(
+                {
+                    title,
+                    html: true,
+                    placement: 'auto',
+                    content,
+                    template: popoverTemplate,
+                })
+            ).popover('show');
+
+        });
     });
 
     function defaultPopoverOptions({content, title, placement = 'top', trigger = 'manual', ...rest}) {
